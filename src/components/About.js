fabcaf6da73ef6b193a1b3432412ca1601039180
Alen Zare Nişan <113252962+nisanalen@users.noreply.github.com>
2023-03-09 14:42:36 +0300

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function About(){
+    return(
+        <div>
+            <h2 className="title">About</h2>
+            <p className="about">I am a student with a particular interest in coding. I also working in a company called Midas as IT analysys. I am always try to get better. My greatest fear is living beneath my potential.</p>
+            <br></br>
+            <h2 className="title">Interests</h2>
+            <p className="about">I love doing sports. Sport's dicipline is not important. I like doing all sports. I like listening rock music.</p>
+        </div>
+    )
+}
